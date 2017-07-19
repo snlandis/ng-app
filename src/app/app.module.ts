c80ba9d2cad1d9658399963fd2be5da255fb849b
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-19 12:39:53 -0500

    rough routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -7,6 +8,12 @@ import { FeaturesComponent } from './features/features.component';
 import { FeaturesCardComponent } from './features-card/features-card.component';
 import { FeatureDetailsComponent } from './feature-details/feature-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: 'home' localhost:3000/home, component: HomeComponent },
+  { path: 'features' localhost:3000/features, component: FeatureComponent },
+];
+
 
 @NgModule({
   declarations: [
@@ -19,6 +26,7 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
