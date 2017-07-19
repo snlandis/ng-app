2d1e801c90608a55b123dc534ed25d168ad40842
Stephen Landis <stephen.n.landis@gmail.com>
2017-07-19 12:51:08 -0500

    removed routing because it was not compiling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from "@angular/router";
+import { Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,11 +8,6 @@ import { FeaturesComponent } from './features/features.component';
 import { FeaturesCardComponent } from './features-card/features-card.component';
 import { FeatureDetailsComponent } from './feature-details/feature-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
-const appRoutes: Routes = [
-  { path: 'home' localhost:3000/home, component: HomeComponent },
-  { path: 'features' localhost:3000/features, component: FeatureComponent },
-];
 
 
 @NgModule({
@@ -26,7 +21,6 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule
-    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
